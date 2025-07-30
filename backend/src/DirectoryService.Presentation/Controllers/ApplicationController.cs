287e9d9e47cf19c00a47940925135ff69fdb99d1
Solovev <solovtev17@bk.ru>
2025-07-30 17:27:24 +0300

    контроллер и реквест

---
+++ backend/src/DirectoryService.Presentation/Controllers/ApplicationController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.Presentation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApplicationController : ControllerBase
+{
+}
---
+++ backend/src/DirectoryService.Presentation/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using DirectoryService.Application.Abstractions;
+using DirectoryService.Application.Locations.Command;
+using DirectoryService.Domain.Error;
+using DirectoryService.Presentation.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.Presentation.Controllers;
+
+public class LocationController : ApplicationController
+{
+    [HttpPost("/locations")]
+    public async Task<ActionResult<Guid>> CreateAsync(
+        [FromBody] CreateLocationRequest request,
+        [FromServices] ICommandHandler<Guid, List<Error>, CreateLocationCommand> handler)
+    {
+        var requestResult = await handler.HandleAsync(request.ToCommand(), CancellationToken.None);
+
+        if (requestResult.IsFailure == true)
+            return BadRequest(requestResult.Error);
+
+        return Ok(requestResult.Value);
+    }
+}
