603d8a2fb8c7ba0cf1bd785185dd36a2463eac50
Solovev <solovtev17@bk.ru>
2025-07-31 19:39:59 +0300

    Добавил seq and serilog. Добавил seq в docker-compose

--- backend/src/DirectoryService.Presentation/Program.cs
+++ backend/src/DirectoryService.Presentation/Program.cs
@@ -1,7 +1,29 @@
 using DirectoryService.Application.DependencyInjection;
 using DirectoryService.Infrastructure.DependencyInjection;
+using DirectoryService.Presentation.Middlewares;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var seqConnection = builder.Configuration["ConnectionStrings:Seq"] ??
+                    throw new NullReferenceException("Missing SEQ connection string");
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+    .Enrich.WithThreadId()
+    .Destructure.ToMaximumDepth(4)
+    .WriteTo.Console(LogEventLevel.Error)
+    .WriteTo.Seq(seqConnection, LogEventLevel.Information)
+    .Enrich.WithProperty("Application", "DS")
+    .CreateLogger();
+
+builder.Services.AddSerilog();
 
 builder.Services.AddInfrastructureLayer(builder.Configuration);
 builder.Services.AddApplicationLayer();
@@ -19,6 +41,9 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
