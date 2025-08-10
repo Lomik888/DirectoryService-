801c284c87391733b0a85a4ab01c43a833f3ad1d
Solovev <solovtev17@bk.ru>
2025-08-10 17:34:38 +0300

    Полная фича по созданию позиции

---
+++ backend/src/DirectoryService.Application/Positions/Command/CreatePositionCommandValidator.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CSharpFunctionalExtensions;
+using DirectoryService.Application.Departments;
+using DirectoryService.Application.Extations;
+using DirectoryService.Contracts.Requests;
+using DirectoryService.Domain.DepartmentValueObjects;
+using DirectoryService.Domain.Err;
+using DirectoryService.Domain.PositionValueObjects;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace DirectoryService.Application.Positions.Command;
+
+public class CreatePositionCommandValidator : AbstractValidator<CreatePositionCommand>
+{
+    public CreatePositionCommandValidator(
+        IPositionRepository positionRepository,
+        IDepartmentsRepository departmentsRepository)
+    {
+        RuleFor(x => x.Request.Name).MustBeValueObject(x => PositionName.Create(x));
+        RuleFor(x => x.Request.Description).MustBeValueObject(x => Description.Create(x));
+
+        RuleFor(x => x.Request.DepartmentIds).NotNull().NotEmpty()
+            .WithMessage(GeneralErrors.Validation.InvalidField(
+                "Коллекция DepartmentIds не может быть пустой",
+                "invalid.request.field"));
+
+        RuleFor(x => x.Request.DepartmentIds).Must(x => x.Count == x.Distinct().Count())
+            .WithMessage(GeneralErrors.Validation.InvalidField(
+                "Коллекция DepartmentIds имеет дубликаты",
+                "invalid.request.field"));
+
+        RuleForEach(x => x.Request.DepartmentIds)
+            .MustBeValueObject(x => DepartmentId.Create(x))
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Request.Name)
+                    .MustAsync(async (name, cancellationToken) =>
+                    {
+                        var positionName = PositionName.Create(name).Value;
+
+                        var exists = await positionRepository.NameIsUniqueAsync(positionName, cancellationToken);
+                        return exists;
+                    })
+                    .WithMessage(GeneralErrors.Validation.InvalidField(
+                        "Активная позиция с таким именем уже существует",
+                        "position.name.exists"))
+                    .DependentRules(() =>
+                    {
+                        RuleFor(x => x.Request.DepartmentIds)
+                            .MustCustomAsync(async (ids, cancellationToken) =>
+                            {
+                                var idsVo = ids.Select(x => DepartmentId.Create(x).Value).ToList();
+
+                                var exists = await departmentsRepository.DepartmentsIsActiveAndExistsAsync(
+                                    idsVo,
+                                    cancellationToken);
+                                if (exists.Count != idsVo.Count)
+                                {
+                                    var invalidIds = idsVo.Except<DepartmentId>(exists).ToList();
+
+                                    var idsMessage = string.Join(", ", invalidIds.Select(x => x.Value));
+                                    var error = Error.Create(
+                                        $"Ids {idsMessage} не являются активными или не сущессвуют",
+                                        "invalid.departments.ids", ErrorTypes.VALIDATION);
+                                    return error;
+                                }
+
+                                return UnitResult.Success<Error>();
+                            });
+                    });
+            });
+    }
+}
---
+++ backend/src/DirectoryService.Application/Positions/Command/CreatePositionHandler.cs
@@ -0,0 +1,66 @@
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Application.Abstractions;
+using DirectoryService.Application.Extations;
+using DirectoryService.Domain;
+using DirectoryService.Domain.Abstractions;
+using DirectoryService.Domain.DepartmentValueObjects;
+using DirectoryService.Domain.Err;
+using DirectoryService.Domain.Extations;
+using DirectoryService.Domain.PositionValueObjects;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+
+namespace DirectoryService.Application.Positions.Command;
+
+public class CreatePositionHandler : ICommandHandler<Guid, Errors, CreatePositionCommand>
+{
+    private readonly IValidator<CreatePositionCommand> _validator;
+    private readonly ILogger<CreatePositionHandler> _logger;
+    private readonly IClock _clock;
+    private readonly IPositionRepository _positionRepository;
+
+    public CreatePositionHandler(
+        IValidator<CreatePositionCommand> validator,
+        ILogger<CreatePositionHandler> logger,
+        IClock clock,
+        IPositionRepository positionRepository)
+    {
+        _validator = validator;
+        _logger = logger;
+        _clock = clock;
+        _positionRepository = positionRepository;
+    }
+
+    public async Task<Result<Guid, Errors>> HandleAsync(
+        CreatePositionCommand command,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(command, cancellationToken);
+        if (validationResult.IsValid == false)
+        {
+            _logger.LogInformation("Invalid validation");
+            return Errors.Create(validationResult.Errors.ToErrors());
+        }
+
+        var positionName = PositionName.Create(command.Request.Name).Value;
+        var description = command.Request.Description == null
+            ? null
+            : Description.Create(command.Request.Description).Value;
+
+        var positionResult = Position.Create(positionName, description, _clock);
+        if (positionResult.IsFailure == true)
+        {
+            return positionResult.Error.ToErrors();
+        }
+
+        var departments = command.Request.DepartmentIds.Select(x => DepartmentId.Create(x).Value);
+
+        var position = positionResult.Value;
+        position.AddDepartments(departments);
+
+        await _positionRepository.AddAsync(position, cancellationToken);
+        await _positionRepository.SaveChangesAsync(cancellationToken);
+
+        return position.Id.Value;
+    }
+}
