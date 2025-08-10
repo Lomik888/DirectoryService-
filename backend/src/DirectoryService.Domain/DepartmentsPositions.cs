81094e3ffe80455cc0b0370b8d818c3b5985250e
Solovev <solovtev17@bk.ru>
2025-08-10 17:35:08 +0300

    новые связачные сущности и их конфигурация DepartmentsLocations и DepartmentsPositions

---
+++ backend/src/DirectoryService.Domain/DepartmentsPositions.cs
@@ -0,0 +1,24 @@
+﻿using DirectoryService.Domain.DepartmentValueObjects;
+using DirectoryService.Domain.PositionValueObjects;
+
+namespace DirectoryService.Domain;
+
+public class DepartmentsPositions
+{
+    public Guid Id { get; set; }
+
+    public DepartmentId DepartmentId { get; set; }
+
+    public PositionId PositionId { get; set; }
+
+    private DepartmentsPositions()
+    {
+    }
+
+    public DepartmentsPositions(DepartmentId departmentId, PositionId positionId)
+    {
+        Id = Guid.NewGuid();
+        DepartmentId = departmentId;
+        PositionId = positionId;
+    }
+}
---
+++ backend/src/DirectoryService.Infrastructure/Configuration/DepartmentsLocationsConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using DirectoryService.Domain;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.Configuration;
+
+public class DepartmentsLocationsConfiguration : IEntityTypeConfiguration<DepartmentsLocations>
+{
+    public void Configure(EntityTypeBuilder<DepartmentsLocations> builder)
+    {
+        builder.ToTable("departments_locations", "department");
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => new { x.DepartmentId, x.LocationId }).IsUnique();
+
+        builder.Property(x => x.Id)
+            .IsRequired()
+            .HasColumnName("id");
+
+        builder.Property(x => x.DepartmentId)
+            .IsRequired()
+            .HasColumnName("department_id");
+
+        builder.Property(x => x.LocationId)
+            .IsRequired()
+            .HasColumnName("location_id");
+
+        builder.HasOne<Department>()
+            .WithMany(x => x.DepartmentsLocations)
+            .HasForeignKey(x => x.DepartmentId);
+
+        builder.HasOne<Location>()
+            .WithMany(x => x.DepartmentsLocations)
+            .HasForeignKey(x => x.LocationId);
+    }
+}
