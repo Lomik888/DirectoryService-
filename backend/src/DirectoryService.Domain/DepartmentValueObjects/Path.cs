a4c61568d338a01ffdfa952368772bbef9595e2f
Solovev <solovtev17@bk.ru>
2025-08-14 14:39:56 +0300

    Добавил методы для VO и Entity Department

--- backend/src/DirectoryService.Domain/DepartmentValueObjects/Path.cs
+++ backend/src/DirectoryService.Domain/DepartmentValueObjects/Path.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using CSharpFunctionalExtensions;
 using DirectoryService.Domain.Err;
+using DirectoryService.Domain.Extations;
 
 namespace DirectoryService.Domain.DepartmentValueObjects;
 
@@ -71,6 +72,35 @@ public class Path : ValueObject
         return new Path(newPathString);
     }
 
+    public static Result<Path, Errors> CreateParent(Path path, Identifier identifier)
+    {
+        var departmentPathStrings = path.Value
+            .Split(Separator)
+            .ToList();
+
+        string newPath;
+        Path newDepartmentPath;
+
+        var indexOldParent = departmentPathStrings.IndexOf(identifier.Value);
+        if (indexOldParent == -1)
+        {
+            var error = GeneralErrors.Validation.InvalidField(
+                "departmentIdentifier.Value не найден в department.Value",
+                "department.invalid.path");
+            return error.ToErrors();
+        }
+
+        if (indexOldParent == 0)
+        {
+            return path;
+        }
+
+        var newDepartmentPathStrings = departmentPathStrings.Skip(indexOldParent - 1);
+        newPath = string.Join(Separator, newDepartmentPathStrings);
+        newDepartmentPath = new Path(newPath);
+
+        return newDepartmentPath;
+    }
 
     public UnitResult<Error> ValidateChildPath(Identifier identifier)
     {
@@ -83,6 +113,40 @@ public class Path : ValueObject
         }
 
         return UnitResult.Success<Error>();
+    }
+
+    public static Result<Path, Errors> Create(Path parent, Path department, Identifier departmentIdentifier)
+    {
+        var departmentPathStrings = department.Value
+            .Split(Separator)
+            .ToList();
+
+        string newPath;
+        Path newDepartmentPath;
+
+        var indexOldParent = departmentPathStrings.IndexOf(departmentIdentifier.Value);
+        if (indexOldParent == -1)
+        {
+            var error = GeneralErrors.Validation.InvalidField(
+                "departmentIdentifier.Value не найден в department.Value",
+                "department.invalid.path");
+            return error.ToErrors();
+        }
+
+        if (indexOldParent == 0)
+        {
+            newPath = $"{parent.Value}{department.Value}";
+            newDepartmentPath = new Path(newPath);
+
+            return newDepartmentPath;
+        }
+
+        var newDepartmentPathStrings = departmentPathStrings.Skip(indexOldParent - 1);
+        var newDepartmentPathString = string.Join(Separator, newDepartmentPathStrings);
+        newPath = $"{parent.Value}{newDepartmentPathString}";
+        newDepartmentPath = new Path(newPath);
+
+        return newDepartmentPath;
     }
 
     public short GetDepth()
